f5e020eb19bd6e04f58cc3f9d2e507acdcd3f622
Takeshita-a <felixjp.takeshita@gmail.com>
2025-12-17 12:07:03 +0900

    プライバシーポリシー、利用規約ページの追加

---
+++ page-privacy.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html> 
+<html lang="ja">
+<head>
+
+<!-- google analytics ここから -->
+<?php if(is_public_server()): ?>
+<?php include get_template_directory()."/_old_assets/_google_tag.php"; ?>
+<?php endif; ?>
+<!-- google analytics ここまで -->
+
+<meta charset="UTF-8">
+<title>プライバシーポリシー | アルトリスト株式会社</title>
+<meta name="keywords" content="アルトリスト, プライバシーポリシー, 相模原技術センター, ステンレス排水溝, 食品, 飲料, 工場, 食品工場, 飲料工場, 包装, マテハン, 設備, 排水溝, 集水, ロボット" />
+<meta name="Description" content="アルトリスト株式会社は、お客様の個人情報の重要性を認識し、個人情報保護に関する法令及びその他の規範を遵守し、以下のとおりプライバシーポリシーを定め、個人情報の適切な管理及び利用に努めます。" />
+
+<?php include get_template_directory()."/_old_assets/_head.php"; ?>
+
+<!--ogp-->
+<meta property="og:title" content="プライバシーポリシー">
+<meta property="og:type" content="website">
+<meta property="og:description" content="アルトリスト株式会社は、お客様の個人情報の重要性を認識し、個人情報保護に関する法令及びその他の規範を遵守し、以下のとおりプライバシーポリシーを定め、個人情報の適切な管理及び利用に努めます。"/>
+<meta property="og:url" content="https://www.altruist.co.jp/privacy.php"/>
+<meta property="og:image" content="https://www.altruist.co.jp/img/ogp_Altruist_2025.png">
+<meta property="og:site_name" content="アルトリスト株式会社"/>
+<!--ogp-->
+
+</head>
+<body>
+
+<div id="style01" class="">
+<?php include get_template_directory()."/_old_assets/_header.php"; ?>
+	<main>
+		<div class="ul_title">
+			<div class="title01">
+				<h2 class="title01_01">プライバシーポリシー</h2>
+			</div>
+		</div>
+		<div class="pankuzu_wrap">
+			<div class="pankuzu"><a href="./">TOP</a>　＞　プライバシーポリシー</div>
+		</div>
+		<section class="res01">
+			<div class="res01_box">
+				<p>アルトリスト株式会社（以下「当社」といいます。）は、お客様の個人情報の重要性を認識し、個人情報保護に関する法令及びその他の規範を遵守し、以下のとおりプライバシーポリシー（以下「本ポリシー」といいます。）を定め、個人情報の適切な管理及び利用に努めます。</p>
+
+				<h3 class="res01_title01">1. 基本方針</h3>
+					<p>当社は、個人情報保護法および関連法令を遵守し、お客様の個人情報を適切に管理します。当社は、お客様の個人情報を、明示した利用目的の範囲内で取り扱い、安全管理措置を講じます。</p>
+
+				<h3 class="res01_title01">2. 取得する個人情報</h3>
+					<p>当社が取得するお客様の個人情報は、以下のとおりです。</p>
+						<ul class="res01_ul01">
+							<li>氏名</li>
+							<li>メールアドレス</li>
+							<li>住所</li>
+							<li>生年月日</li>
+							<li>電話番号</li>
+							<li>お問い合わせ内容</li>
+							<li>その他当社が提供するサービスのご利用状況等</li>
+						</ul>
+
+				<h3 class="res01_title01">3. 個人情報の利用目的</h3>
+					<p>当社は、取得した個人情報を以下の目的で利用します。</p>
+						<ol class="res01_ul01">
+							<li>お客様へのサービス提供、商品の発送、関連するアフターサービスのため</li>
+							<li>お客様からのお問い合わせ、ご相談への対応のため</li>
+							<li>当社のサービス、商品、イベント等に関する情報提供のため</li>
+							<li>サービス品質の向上、改善のための分析、調査のため</li>
+							<li>不正行為、不正アクセス等の防止のため</li>
+							<li>お取引さまとの契約関係の管理のため</li>
+							<li>従業員等の雇用（採用を含みます）および人事管理のため</li>
+							<li>その他、上記に付随する目的のため</li>
+						</ol>
+
+				<h3 class="res01_title01">4. 個人情報の第三者提供</h3>
+					<p>当社は、以下の場合を除き、お客様の同意を得ることなく個人情報を第三者に提供することはありません。</p>
+						<ol class="res01_ol01">
+							<li>お客様の同意がある場合</li>
+							<li>法令に基づく場合</li>
+							<li>人の生命、身体または財産の保護のために必要がある場合であって、お客様の同意を得ることが困難であるとき</li>
+							<li>利用目的の達成に必要な範囲内において、個人情報の取扱いの全部または一部を委託する場合</li>
+						</ol>
+
+				<h3 class="res01_title01">5. 個人情報の委託</h3>
+					<p>当社は、利用目的の達成に必要な範囲内において、個人情報の取扱いの全部または一部を外部に委託する場合があります。委託先については、個人情報の適切な管理体制を有する者を選定し、委託契約において安全管理に関する事項等を定め、適切な監督を行います。</p>
+
+				<h3 class="res01_title01">6. 個人情報の管理</h3>
+					<p>当社は、お客様の個人情報を適切に管理し、不正アクセス、紛失、破壊、改ざん及び漏洩等の防止のために、必要かつ適切な安全管理措置を講じます。</p>
+
+				<h3 class="res01_title01">7. Cookie（クッキー）及びアクセスログについて</h3>
+						<ol class="res01_ul01">
+							<li><span class="bold">Cookieの利用</span><br />
+							当社のウェブサイトでは、お客様の利便性向上、サイト改善、およびお客様に適切な情報提供を行うため、Cookieを使用しています。Cookieはお客様のコンピューターを識別するものであり、個人を特定する情報は含まれません。お客様は、ブラウザの設定によりCookieの受け取りを拒否することができますが、その場合、ウェブサイトの一部機能が利用できなくなる可能性があります。</li>
+							<li><span class="bold">アクセスログの取得</span><br />
+							当社のウェブサイトでは、アクセスされた方の情報をアクセスログとして記録しています。アクセスログには、アクセスされた方のIPアドレス、ブラウザの種類、アクセス日時などが含まれますが、個人を特定できる情報は含まれません。アクセスログは、ウェブサイトの利用状況分析やサーバーの問題解決のために利用されます。</li>
+						</ol>
+
+				<h3 class="res01_title01">8. 個人情報の開示、訂正、利用停止等の請求</h3>
+					<p>お客様は、当社が保有するご自身の個人情報について、開示、訂正、追加、削除、利用停止、消去、第三者提供の停止を請求することができます。ご請求の際は、ご本人であることを確認させていただいた上で、法令に基づき適切に対応いたします。</p>
+
+				<h3 class="res01_title01">9. お問い合わせ窓口</h3>
+					<p>当社の個人情報の取扱いに関するお問い合わせ、ご請求は、以下の窓口までご連絡ください。</p>
+						<p>
+							アルトリスト株式会社<br />
+							部署名：管理部<br />
+							電話番号：042-444-2477<br />
+							メールアドレス：info@altruist.co.jp
+						</p>
+
+				<h3 class="res01_title01">10. 本ポリシーの変更</h3>
+					<p>当社は、法令の改正その他必要に応じて、本ポリシーを予告なく変更することがあります。変更後の本ポリシーは、当社のウェブサイトに掲載した時点から効力を生じるものとします。</p>
+				
+				<p class="txt_r">制定日：2008年10月21日<br />
+				改訂日：2025年 6月 6日</p>
+
+			</div>
+		</section>
+<?php include get_template_directory()."/_old_assets/_footer.php"; ?>
+</div>
+
+<?php include get_template_directory()."/_old_assets/_js.php"; ?>
+
+<?php include get_template_directory()."/_old_assets/_cookie.php"; ?>
+</body>
+</html>
