f17ad2d989006cb6b8ebdfcaa74617345eeaadf9
Takeshita-a <felixjp.takeshita@gmail.com>
2025-12-17 12:06:05 +0900

    プライバシーポリシー、利用規約ページの追加

--- _old_assets/_header.php
+++ _old_assets/_header.php
@@ -5,12 +5,12 @@
 		<nav class="header_nav">
 			<ul class="header_nav01">
 				<li><a href="<?php echo home_url() ?>">TOP</a></li>
-				<li><a href="<?php echo home_url('/company/') ?>">企業情報</a></li>
-				<li><a href="<?php echo home_url('/equipment/') ?>">FA技術営業</a></li>
-				<li><a href="<?php echo home_url('/engineering/') ?>">プラントエンジニアリング</a></li>
-				<li><a href="<?php echo home_url('/robots/') ?>">技術（相模原技術センター）</a></li>
-				<li><a href="<?php echo home_url('/products/') ?>">製品情報</a></li>
-				<li><a href="https://en-gage.net/altruist_saiyo/" target="_blank">採用情報<img class="nav01_icon" src="<?php echo get_template_directory_uri() ?>/_old_assets/img/gaibu_link.png" /></a></li>
+				<!-- <li><a href="<?php echo home_url('/company/') ?>">企業情報</a></li> -->
+				<!-- <li><a href="<?php echo home_url('/equipment/') ?>">FA技術営業</a></li> -->
+				<!-- <li><a href="<?php echo home_url('/engineering/') ?>">プラントエンジニアリング</a></li> -->
+				<!-- <li><a href="<?php echo home_url('/robots/') ?>">技術（相模原技術センター）</a></li> -->
+				<!-- <li><a href="<?php echo home_url('/products/') ?>">製品情報</a></li> -->
+				<!-- <li><a href="https://en-gage.net/altruist_saiyo/" target="_blank">採用情報<img class="nav01_icon" src="<?php echo get_template_directory_uri() ?>/_old_assets/img/gaibu_link.png" /></a></li> -->
 				<li class="pc_none"><a href="<?php echo home_url('/terms/') ?>">サイト利用規約</a></li>
 				<li class="pc_none"><a href="<?php echo home_url('/privacy/') ?>">プライバシーポリシー</a></li>
 				<li class="pc_none"><a href="<?php echo home_url('/contact/') ?>">お問合せ</a></li>
