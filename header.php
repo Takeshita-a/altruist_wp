f9837493b510eb5282e9abc4cf8e5df0ce705357
Takeshita-a <felixjp.takeshita@gmail.com>
2025-12-25 18:35:36 +0900

    header関連構成修正

--- header.php
+++ header.php
@@ -3,9 +3,6 @@
 <!DOCTYPE html>
 <html lang="jp">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php my_page_title(); ?></title>
-	<?php wp_head(); ?>
+	<?php require_once get_template_directory() . '/inc/head.php'; ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
---
+++ inc/head.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php my_page_title(); ?></title>
+<?php wp_head(); ?>
