81be3a84a2d4cc90f330994ddcfc680741d402c2
Takeshita-a <felixjp.takeshita@gmail.com>
2025-12-26 17:09:10 +0900

    goolgleフォントCDN追加

--- inc/head.php
+++ inc/head.php
@@ -1,4 +1,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- FONT -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+<!-- //FONT -->
 <title><?php my_page_title(); ?></title>
 <?php wp_head(); ?>
