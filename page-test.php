3cc8f4ecd658dd47c7ce56bc2b704955079cccde
Takeshita-a <felixjp.takeshita@gmail.com>
2026-01-05 15:10:26 +0900

    パーツ作成、サンプルページ追加

---
+++ page-test.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+
+<div class="common-ttl">
+	<h2 class="common-ttl-en">NEWS</h2>
+	<p class="common-ttl-ja">ニュース</p>
+</div>
+
+<div class="common-ttl v2">
+	<h2 class="common-ttl-en">ABOUT US</h2>
+	<p class="common-ttl-ja">アルトリストについて</p>
+</div>
+
+<a href="" class="common-btn news-btn">
+	<p class="common-btn-txt">ニュース一覧</p>
+	<i class="common-btn-arrow"></i>
+</a>
+
+
+
+<?php get_footer(); ?>
